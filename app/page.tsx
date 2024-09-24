762f3e862c58421e9d90d1715fc86315e5ed18e1
zeyuanzhao <14340342+zeyuanzhao@users.noreply.github.com>
2024-09-24 11:54:52 -0400

    test states

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,36 @@
+"use client";
+
 import { Flashcard } from "@/interfaces";
 import { useState } from "react";
 
 const Home = () => {
-  const [flashcards, setFlashcards] = useState<Flashcard[]>();
-  return <h1>Quizlet</h1>;
+  const [flashcards, setFlashcards] = useState<Flashcard[]>([
+    { definition: "A", term: "B" },
+    { definition: "C", term: "D" },
+    { definition: "E", term: "F" },
+  ]);
+  const [side, setSide] = useState<"term" | "definition">("term");
+
+  return (
+    <div className="px-4 py-8">
+      <h1 className="text-2xl">Quizlet</h1>
+      <button
+        onClick={() =>
+          side === "term" ? setSide("definition") : setSide("term")
+        }
+      >
+        {side}
+      </button>
+      {flashcards.map((card) => {
+        return (
+          <>
+            <h2>{card.term}</h2>
+            <p>{card.definition}</p>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
